a35e648443fc11b3102f344dc3a835223638991f
rafelsoutoo <rafaelsouto70017@gmail.com>
2024-07-24 02:44:54 -0300

    temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import {VStack } from "@chakra-ui/react";
+import { Divider, VStack } from "@chakra-ui/react";
 import { Header } from "@/Components/Header";
 import { NewArticles } from "@/Components/NewArticles";
 
 export default function Index() {
   return (
     <VStack >
-      <Header/>
-      <NewArticles/>
+      <Header />
+      <NewArticles />
+      <Divider borderColor="#C5CAC5" w="65%"  mt={100}/>
     </VStack>
   )
 }
